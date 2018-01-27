bd051ea87261f7a2c68636888fdb4a146471ce5e
teja nakka <tejanakka1@gmail.com>
2018-01-27 04:53:56 -0800

    login page working with firebase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 var modal = document.getElementById('simplemodal');
 var modalbtn = document.getElementById('modalbtn');
 var closebtn = document.getElementById('btn1');
@@ -16,6 +18,65 @@ function closemodal(){
     modal.style.display = 'none';
     cntnt.style.display='block';
 }
+
+function login(){
+    var userName = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+
+    firebase.auth().onAuthStateChanged(function(user){
+        if(user){
+            window.open("pages/loggedin.html");
+            console.log("user logged in"); //user is sigmed in
+        } else {
+           console.log("user not logged in"); //user is not signed in.
+        }
+    });
+    
+    firebase.auth().signInWithEmailAndPassword(userName, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    
+        window.alert("Error : " + errorMessage);
+        // ...
+      });
+};
+
+function signup(){
+    var signupName = document.getElementById("signupName").value;
+    var signupPass = document.getElementById("signupPass").value;
+
+    firebase.auth().onAuthStateChanged(function(user){
+        if(user){
+            window.alert("account successfully created");
+            open("pages/loggedin.html");
+            console.log("account created"); //user is sigmed in
+        } else {
+           console.log("account not created"); //user is not signed in.
+        }
+    });
+    
+    firebase.auth().createUserWithEmailAndPassword(signupName, signupPass).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        window.alert("Error : " + errorMessage);
+        // ...
+      });
+};
+
+function deleteMe(){
+    console.log("function works");
+    var user = firebase.auth().currentUser;
+
+user.delete().then(function() {
+  window.alert("your account has been deleted");   // User deleted.
+  open("../index.html");
+}).catch(function(error) {
+  // An error happened.
+});
+};
 
 document.addEventListener('DOMContentLoaded', function(){
     let wrapper = document.getElementById('wrapper');
